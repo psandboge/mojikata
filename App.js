13b6409b65e20cf432692e0da4a3d6d408f9a52e
Pierre Sandboge <pierre@sandboge.com>
2018-02-28 12:37:10 +0100

    Clean up a bit

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Button, Dimensions, PanResponder, StyleSheet, View } from 'react-native';
-import Svg, {Circle, G, Path, Text as Txt} from 'react-native-svg';
+import {Button, Dimensions, PanResponder, StyleSheet, View} from 'react-native';
+import Svg, {Circle, Path} from 'react-native-svg';
 import Kana from "./components/Kana";
 
-const index = require("./assets/json/hiragana_index.json")
-const { width, height } = Dimensions.get('window');
-const svgSize = width*0.9
-//const yoff = height/6
-//const xoff = (width - svgSize)/2
+const index = require("./assets/json/hiragana_index.json");
+const {width, height} = Dimensions.get('window');
+const svgSize = width < height ? width * 0.9 : height * 0.9;
 export default class App extends React.Component {
-    constructor () {
+    constructor() {
         super();
-//        console.log("yoff " + yoff)
-//        console.log("xoff " + xoff)
-        console.log("width" + width)
+        console.log("width" + width);
         this.state = {
             xoff: 0,
             yoff: 0,
@@ -27,50 +23,13 @@ export default class App extends React.Component {
             cnt: 412,
             showMaster: false,
             svg1: <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}/>,
-            svgMaster: <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}>
-                <G scale={svgSize/109}>
-                <G id="StrokePaths_04e07" stroke="red" fill="none" stroke-width="13" style="fill:none;stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;">
-                    <G id="04e07" element="万">
-                        <G id="04e07-g1" element="一" radical="general">
-                            <Path id="04e07-s1" type="㇐" d="M14.38,24.73c2.3,0.54,6.52,0.78,8.81,0.54c21.57-2.27,44.44-5.64,64.9-5.98c3.83-0.06,6.12,0.26,8.04,0.53"/>
-                        </G>
-                        <Path id="04e07-s2" type="㇆" d="M51,41.5c1.45,0.7,3.19,1.43,5.19,1.74c7.31,1.14,17.05,1.94,22.64,1.5c4.64-0.37,6.38,1.08,5.17,4.73C77.88,68,72.75,78.75,63.87,90.4c-7.6,9.97-10.12,3.22-12.62,0.2"/>
-                        <Path id="04e07-s3" type="㇒" d="M51.75,25.5c0.5,2,0.22,3.78-0.21,5.89C48.95,43.8,34.75,73.38,13.56,87.97"/>
-                    </G>
-                </G>
-                <G id="StrokeNumbers_04e07" style="font-size:8;fill:#808080">
-                    <Txt
-                        fill="none"
-                        stroke="purple"
-                        fontSize="10"
-                        fontWeight="bold"
-                        x="7.50"
-                        y="26.50"
-                        textAnchor="middle">1</Txt>
-                    <Txt
-                        fill="none"
-                        stroke="purple"
-                        fontSize="10"
-                        fontWeight="bold"
-                        x="57.50"
-                        y="41.50"
-                        textAnchor="middle">2</Txt>
-                    <Txt
-                        fill="none"
-                        stroke="purple"
-                        fontSize="10"
-                        fontWeight="bold"
-                        x="43.50"
-                        y="33.50"
-                        textAnchor="middle">3</Txt>
-                </G>
-                </G>
-            </Svg>
-    };
-        this._handlePanResponderMove = this._handlePanResponderMove.bind(this)
+            svgMaster: <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}/>
+        };
+        this._handlePanResponderMove = this._handlePanResponderMove.bind(this);
 
     }
-    componentWillMount () {
+
+    componentWillMount() {
         this._panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: this._handlePanResponderMove,
@@ -78,23 +37,21 @@ export default class App extends React.Component {
         });
     }
 
-    _handlePanResponderMove (evt, gestureState) {
-        let xdiff = gestureState.x0 - gestureState.moveX
-        let ydiff = gestureState.y0 - gestureState.moveY
-        let x = gestureState.moveX
-        let y = gestureState.moveY
+    _handlePanResponderMove(evt, gestureState) {
+        let x = gestureState.moveX;
+        let y = gestureState.moveY;
 
         let ps = this.state.ps;
         let cx = this.state.cx;
         let cy = this.state.cy;
         this.state.cnt += 1;
 
-        cx.push(x - this.state.xoff)//scale;
+        cx.push(x - this.state.xoff);
         cy.push(y - this.state.yoff);
         if (ps.length > 35) {
-            ps.splice(17,1);
-            cx.splice(17,1);
-            cy.splice(17,1);
+            ps.splice(17, 1);
+            cx.splice(17, 1);
+            cy.splice(17, 1);
         }
 
         let l1 = null;
@@ -103,14 +60,32 @@ export default class App extends React.Component {
             for (let x = 1; x < cx.length; x++) {
                 lcs = lcs + "L" + cx[x] + "," + cy[x];
             }
-            l1 = React.createElement(Path, {key:"p" + this.state.cnt, d:lcs, fill:"none", stroke:"green", strokeWidth:"4"})
-            ps = React.createElement(Path, {key:"p" + this.state.cnt, d:lcs, fill:"none", stroke:"blue", strokeWidth:"4"})
+            l1 = React.createElement(Path, {
+                key: "p" + this.state.cnt,
+                d: lcs,
+                fill: "none",
+                stroke: "green",
+                strokeWidth: "4"
+            });
+            ps = React.createElement(Path, {
+                key: "p" + this.state.cnt,
+                d: lcs,
+                fill: "none",
+                stroke: "blue",
+                strokeWidth: "4"
+            });
         } else {
-            ps=React.createElement(Circle,  {key:this.state.cnt, cx:x/2-this.state.xoff, cy:y/2-this.state.yoff, r:"2", stroke:"blue"}, "");
+            ps = React.createElement(Circle, {
+                key: this.state.cnt,
+                cx: x / 2 - this.state.xoff,
+                cy: y / 2 - this.state.yoff,
+                r: "2",
+                stroke: "blue"
+            }, "");
         }
 
-        let vbox = "0 0 " + svgSize + " " + svgSize
-        let newSvg = React.createElement(Svg,{width:svgSize, height:svgSize, viewBox:vbox},
+        let vbox = "0 0 " + svgSize + " " + svgSize;
+        let newSvg = React.createElement(Svg, {width: svgSize, height: svgSize, viewBox: vbox},
             this.state.lines.concat(l1));
 
         this.setState({
@@ -120,47 +95,50 @@ export default class App extends React.Component {
                 y: gestureState.moveY
             },
             ps: ps
-        })
+        });
     }
+
     _handlePanResponderRelease = () => {
         let l = this.state.lines.slice();
         l.push(this.state.ps);
         this.setState({
             lines: l,
-            ps:[],
-            cx:[],
-            cy:[]
-        })
+            ps: [],
+            cx: [],
+            cy: []
+        });
+    };
+
+    render() {
+        console.log("glyph: " + this.state.glyph);
+        return (
+            <View style={styles.container}>
+                <View onLayout={(n) => this.getCoordinates(n)} {...this._panResponder.panHandlers} style={styles.svgs}>
+                    <View style={styles.svg1}>
+                        {this.state.svg1}
+                        <View style={this.state.showMaster ? styles.svg2 : styles.svgHidden}>
+                            <Kana width={svgSize} character={this.state.glyph}/>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.brow}>
+                    <View style={styles.text}>
+                        <Button color='#222' title={this.state.text} onPress={(evt) => {
+                        }}/>
+                    </View>
+                    <View style={styles.butt}>
+                        <Button color='#eee' title="Rensa" onPress={(evt) => this.handleResetPress(evt)}/>
+                    </View>
+                    <View style={styles.nextButt}>
+                        <Button color='#eee' title="Nästa" onPress={(evt) => this.handleNext(evt)}/>
+                    </View>
+                    <View style={styles.hideButt}>
+                        <Button color='#eee' title="Visa" onPress={(evt) => this.handleHidePress(evt)}/>
+                    </View>
+                </View>
+            </View>
+        );
     }
-  render() {
-      console.log("glyph: " + this.state.glyph)
-    return (
-      <View style={styles.container}>
-          <View onLayout = {(n) => this.getCoordinates(n)} {...this._panResponder.panHandlers} style={styles.svgs}>
-              <View style = {styles.svg1}>
-                  {this.state.svg1}
-                  <View style = {this.state.showMaster ? styles.svg2: styles.svgHidden}>
-                      <Kana width={svgSize} character = {this.state.glyph}/>
-                  </View>
-              </View>
-          </View>
-          <View style = {styles.brow}>
-          <View style = {styles.text}>
-              <Button color='#222' title = {this.state.text} onPress = {(evt) => {}}/>
-          </View>
-          <View style = {styles.butt}>
-              <Button color='#eee' title = "Rensa" onPress = {(evt) => this.handleResetPress(evt)}/>
-          </View>
-          <View style = {styles.nextButt}>
-              <Button color='#eee' title = "Nästa" onPress = {(evt) => this.handleNext(evt)}/>
-          </View>
-          <View style = {styles.hideButt}>
-              <Button color='#eee' title = "Visa" onPress = {(evt) => this.handleHidePress(evt)}/>
-          </View>
-          </View>
-      </View>
-    );
-  }
 
     handleResetPress(evt) {
         this.setState({
@@ -171,6 +149,7 @@ export default class App extends React.Component {
             svg1: <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}/>,
         })
     }
+
     handleHidePress(evt) {
         this.setState({
             showMaster: !(this.state.showMaster)
@@ -178,7 +157,7 @@ export default class App extends React.Component {
     }
 
     handleNext(evt) {
-        let next = (this.state.current + 1) % index.length
+        let next = (this.state.current + 1) % index.length;
         this.setState({
             current: next,
             text: index[next][0],
@@ -188,12 +167,12 @@ export default class App extends React.Component {
             cx: [],
             cy: [],
             svg1: <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}/>,
-        })
+        });
         console.log("NextState: " + this.state)
     }
 
     getCoordinates(n) {
-        console.log("Coordinates:  " + n.nativeEvent.layout.y)
+        console.log("Coordinates:  " + n.nativeEvent.layout.y);
         this.setState({
             xoff: n.nativeEvent.layout.x,
             yoff: n.nativeEvent.layout.y
@@ -203,12 +182,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-//    position: 'absolute',
         flex: 1,
-//      top:0,
-//      left:0,
-//        height:height,
-//      width:width,
         backgroundColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
@@ -221,42 +195,24 @@ const styles = StyleSheet.create({
     },
     text: {
         margin: 3,
-        // position: 'absolute',
-        // top:height/1.5 -40,
-        // left:width/8,
         backgroundColor: '#ee0',
     },
     butt: {
         margin: 3,
-        // position: 'absolute',
-        // top:height/1.5,
-        // left:width/8,
         backgroundColor: '#b23',
     },
     hideButt: {
         margin: 3,
-        // position: 'absolute',
-        // top:height/1.5,
-        // right:width/8,
         backgroundColor: '#b23',
     },
     nextButt: {
         margin: 3,
-        // position: 'absolute',
-        // top:height/1.5,
-        // right:width/8*3,
         backgroundColor: '#b23',
     },
     svgs: {
-        // position:'absolute',
-        // top:yoff,
-        // left:xoff,
         backgroundColor: '#fff',
     },
     svg1: {
-//        position:'absolute',
-//        top:0,
-//        left:0,
         backgroundColor: '#eeb',
 
         transform: [
@@ -264,16 +220,16 @@ const styles = StyleSheet.create({
                 scale: 1
             },
             {
-                translateX :0
+                translateX: 0
             },
-            { translateY:0}
+            {translateY: 0}
 
         ]
     },
     svg2: {
-        position:'absolute',
-        top:0,
-        left:0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
         backgroundColor: '#f8d0',
 
         transform: [
@@ -281,16 +237,16 @@ const styles = StyleSheet.create({
                 scale: 1
             },
             {
-                translateX :0
+                translateX: 0
             },
-            { translateY:0}
+            {translateY: 0}
 
         ]
     },
     svgHidden: {
-        position:'absolute',
-        top:0,
-        left:0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
         backgroundColor: '#a8df',
 
         transform: [
@@ -298,9 +254,9 @@ const styles = StyleSheet.create({
                 scale: 1
             },
             {
-                translateX :-1000
+                translateX: -1000
             },
-            { translateY:0}
+            {translateY: 0}
 
         ]
     }
--- components/Kana.js
+++ components/Kana.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
 import {Svg, G, Path, Text} from 'react-native-svg';
 
-const index = require("../assets/json/hiragana_index.json")
-const chars = require("../assets/json/hiragana.json")
+const index = require("../assets/json/hiragana_index.json");
+const chars = require("../assets/json/hiragana.json");
 
 export default class Kana extends React.Component {
     constructor(props) {
@@ -13,18 +12,17 @@ export default class Kana extends React.Component {
             texts: [],
         };
         svgSize = props.width;
-        console.log(svgSize)
-//       console.log(props)
+        console.log(svgSize);
         this.getSvg(props);
     }
 
     getSvg(props) {
-        this.state.lines = []
-        this.state.texts = []
-        let character = chars.hiragana[props.character]
-        console.log("Char: " + character)
-        let paths = character.path
-        let texts = character.text
+        this.state.lines = [];
+        this.state.texts = [];
+        let character = chars.hiragana[props.character];
+        console.log("Char: " + character);
+        let paths = character.path;
+        let texts = character.text;
         for (let i = 0; i < paths.length; i++) {
             this.state.lines = this.state.lines.concat(React.createElement(Path, {
                 key: "s" + i,
@@ -33,34 +31,31 @@ export default class Kana extends React.Component {
                 stroke: "red",
                 strokeWidth: "1"
             }))
-//            console.log(this.state.lines)
         }
         for (let i = 0; i < texts.length; i++) {
             let raw = texts[i].transform;
-//            console.log(raw)
-            let s = raw.split(' ')
-            let x = s[4]
-            let y = s[5].substring(0, s[5].length - 1)
-            let t = texts[i].value
+            let s = raw.split(' ');
+            let x = s[4];
+            let y = s[5].substring(0, s[5].length - 1);
+            let t = texts[i].value;
             this.state.texts = this.state.texts.concat(React.createElement(Text, {
                 key: "t" + i, fill: "orange", stroke: "orange", fontSize: "8",
                 x: x,
                 y: y,
                 textAnchor: "middle"
             }, t))
-//            console.log(this.state.texts)
         }
     }
 
     componentWillUpdate(nextProps, nextState) {
-        console.log("next:" + nextProps.character)
+        console.log("next:" + nextProps.character);
         this.getSvg(nextProps)
     }
 
     render() {
         return <Svg width={svgSize} height={svgSize} viewBox={"0 0 " + svgSize + " " + svgSize}>
-            <G scale={svgSize/109}>
-                <G stroke="red" fill="none" stroke-width="13" >
+            <G scale={svgSize / 109}>
+                <G stroke="red" fill="none" stroke-width="13">
                     {this.state.lines}
                 </G>
                 <G>
